2892a3f1119ed5001b3300fd85bc62f27581c9cd
karthik-140 <shanigaramkarthik@gmail.com>
2023-01-12 14:43:59 +0530

    axios

--- main.js
+++ main.js
@@ -1,51 +1,157 @@
 // GET REQUEST
+axios.defaults.headers.common['x-Auth-Token']=
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 function getTodos() {
-  console.log('GET Request');
+  //  axios({
+  //   method:'get',
+  //   url:'https://jsonplaceholder.typicode.com/todos',
+  //   params: {
+  //     _limit: 5
+  //   }
+  //  })
+  //  .then(res => showOutput(res))
+  //  .catch(err => console.log(err))
+
+  axios
+   .get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+   .then(res => showOutput(res))
+   .catch(error=>console.log(error))
 }
 
 // POST REQUEST
 function addTodo() {
-  console.log('POST Request');
+  axios
+  .post('https://jsonplaceholder.typicode.com/todos',{
+    title:'New data',
+    completed:false
+  })
+  .then(res => showOutput(res))
+  .catch(err=>console.log(err))
 }
 
 // PUT/PATCH REQUEST
 function updateTodo() {
-  console.log('PUT/PATCH Request');
+axios
+.patch('https://jsonplaceholder.typicode.com/todos/1',{
+  title:'New data',
+  completed:true
+})
+.then(res => showOutput(res))
+.catch(err=>console.log(err))
 }
 
 // DELETE REQUEST
 function removeTodo() {
-  console.log('DELETE Request');
+ axios
+ .delete('https://jsonplaceholder.typicode.com/todos/1')
+ .then(res=>showOutput(res))
+ .catch(err=>console.log(err))
 }
 
 // SIMULTANEOUS DATA
 function getData() {
-  console.log('Simultaneous Request');
+  axios.all([
+    axios.get('https://jsonplaceholder.typicode.com/todos'),
+    axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')
+  ])
+  .then(axios.spread((todos,posts)=>showOutput(posts))
+  
+  )
 }
 
 // CUSTOM HEADERS
 function customHeaders() {
-  console.log('Custom Headers');
+  const config={
+    headers:{
+      'Content-type':'application/json',
+      authorization:'sometoken'
+    }
+  }
+  axios.post('https://jsonplaceholder.typicode.com/todos',{
+    title:'New todo',
+    completed:false
+  },config)
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err))
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
 function transformResponse() {
-  console.log('Transform Response');
+  const options={
+    method:'post',
+    url:'https://jsonplaceholder.typicode.com/todos',
+    data:{
+      title:'Hello world'
+    },
+    transformResponse:axios.defaults.transformResponse.concat(data=>{
+      data.title=data.title.toUpperCase();
+      return data;
+    })
+  }
+  axios(options).then(res=>showOutput(res))
 }
 
 // ERROR HANDLING
 function errorHandling() {
-  console.log('Error Handling');
+ axios
+ .get('https://jsonplaceholder.typicode.com/todoss',{
+  //validateStatus: function(status){
+  //  return status <500;
+  // }
+ })
+ .then(res=>showOutput(res))
+ .catch(err=>{
+  if(err.response){
+    console.log(err.response.data)
+    console.log(err.response.status)
+    console.log(err.response.headers)
+    if(err.response.status===404){
+      alert('Error:page not found')
+    }
+  }else if(err.request){
+    console.error(err.request)
+  }else{
+    console.error(err.message)
+  }
+ })
 }
 
 // CANCEL TOKEN
 function cancelToken() {
-  console.log('Cancel Token');
+  const source=axios.CancelToken.source()
+
+  axios
+  .get('https://jsonplaceholder.typicode.com/todos',{
+    cancelToken:source.token
+  })
+  .then(res=>showOutput(res))
+  .catch(thrown=>{
+    if(axios.isCancel(thrown)){
+      console.log('Request cancelled',thrown.message)
+    }
+  })
+  if(true){
+    source.cancel('Request cancelled!')
+  }
 }
 
 // INTERCEPTING REQUESTS & RESPONSES
 
+axios.interceptors.request.use(
+  config=>{
+    console.log(`${config.method.toUpperCase()} request sent to ${config.url} at ${new Date().getTime()}`)
+     return config
+  },error=>{
+    return Promise.reject(error)
+  }
+)
+
 // AXIOS INSTANCES
+
+const axiosInstance=axios.create({
+  baseURL:'https://jsonplaceholder.typicode.com'
+})
+axiosInstance.get('/comments').then(res=>showOutput(res))
 
 // Show output in browser
 function showOutput(res) {
@@ -95,3 +201,15 @@ document
   .addEventListener('click', transformResponse);
 document.getElementById('error').addEventListener('click', errorHandling);
 document.getElementById('cancel').addEventListener('click', cancelToken);
+
+
+/*1)why do we need headers??
+--> we need headers to share data between server and client
+
+ 2)What is axios?
+--> Axios is a tool used by developers by using requests to fetch data,
+   requests may be GET, POST, PUT/PATCH and DELETE.
+   
+3)What are the common problems faced when you make network calls and what should you do to solve it.
+--> The main issue while making network calls is calling by using wrong path, using incorrect syntax's etc..
+*/
